9eb1125a52c5442ced66715e4cb7e48a08bac985
sokpanha10000-oss <sokpanha10000@gmail.com>
2026-09-22 14:08:15 +0700

    Update DarkyHub.lua

--- DarkyHub.lua
+++ DarkyHub.lua
@@ -23,7 +23,7 @@ local Config = {
     Secret          = "1234", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
-    MainScriptURL   = "Link", -- The raw URL of your main script
+    MainScriptURL   = "https://raw.githubusercontent.com/hanniii1/Loader/refs/heads/main/BFLoader.lua", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
